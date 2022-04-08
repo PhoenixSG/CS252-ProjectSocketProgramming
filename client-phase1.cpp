40908af632ef0bb680bfbd000b916ebb5951f07d
Shashwat Garg <shashwatg2002@gmail.com>
2022-04-08 19:35:03 +0530

    Failed fork attempt

--- client-phase1.cpp
+++ client-phase1.cpp
@@ -9,7 +9,6 @@
 #include <sstream>
 #include <thread>
 
-std::mutex mu;
 void tokenize(std::string &str, char delim, std::vector<std::string> &out)
 {
 	size_t start;
@@ -31,120 +30,9 @@ void print_file_names(char **argv)
 	system(commd.c_str());
 }
 
-void server(int port, int num_neighbour)
-{
-
-	int valread;
-
-	char buffer[1024] = {0};
-	// Creating socket file descriptor
-
-	int server_socket = socket(AF_INET, SOCK_STREAM, 0);
-	struct sockaddr_in address;
-	int addrlen = sizeof(address);
-	int yes = 1;
-	std::cout << "Server "
-			  << " made with fd as " << server_socket << "and port " << port << std::endl;
-
-	setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &yes, sizeof(yes));
-	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = INADDR_ANY;
-	address.sin_port = htons(port);
-	bind(server_socket, (struct sockaddr *)&address, sizeof(address));
-	std::set<int> count;
-
-	std::string msg = "nahi mila";
-		const char *msg2 = msg.c_str();
-
-
-	while (count.size() < num_neighbour)
-	{
-		if (listen(server_socket, 10) != -1)
-		{
-
-			std::cout << "Server "
-					  << " listening on port " << port << std::endl;
-
-			int new_client = accept(server_socket, (struct sockaddr *)&address, (socklen_t *)&addrlen);
-			std::cout << "Server accept returned " << new_client << std::endl;
-
-			if (new_client > -1)
-			{
-				std::cout << "Server accepted a connection " << std::endl;
-
-				int client_msg_len = read(new_client, buffer, 1024);
-				if (client_msg_len > 48)
-				{
-					std::cout << "Server received a message " << std::endl;
-
-					printf("%s\n", buffer);
-					printf("Amount of text received-%d\n", client_msg_len);
-					count.insert(atoi(buffer));
-				}
-				else{
-					send(new_client,msg2,strlen(msg2),0);
-				}
-			}
-		}
-	}
-	printf("Server exiting while loop\n");
-
-}
-
-void client(int S_NO, int num_neighbour, std::vector<int> &neighbour_client_port, std::vector<int> &neighbour_client_number, int PORT, int ID)
-{
-
-	std::cout << "Client " << S_NO << " starting up. Num neighbours = "<<num_neighbour << std::endl;
-
-	struct sockaddr_in neighbour_address[num_neighbour];
-	int client_socket[num_neighbour];
-	int status[num_neighbour];
-
-	for (int i = 0; i < num_neighbour; i++)
-	{
-
-		int valread;
-		// std::string msg = std::to_string(S_NO) + " You are " + std::to_string(neighbour_client_number[i]) + ". I am " + std::to_string(S_NO) + ", on port " + std::to_string(PORT) + ",with ID " + std::to_string(ID);
-		std::string msg = std::to_string(S_NO);
-		const char *msg2 = msg.c_str();
-		char buffer[1024] = {0};
-		client_socket[i] = socket(AF_INET, SOCK_STREAM, 0);
-
-		neighbour_address[i].sin_family = AF_INET;
-		neighbour_address[i].sin_port = htons(neighbour_client_port[i]);
-		neighbour_address[i].sin_addr.s_addr = inet_addr("127.0.0.1");
-		printf("Client attempting connect to port %d \n", neighbour_client_port[i]);
-
-		int status = 0;
-		while (status == 0)
-		{
-			status = connect(client_socket[i], (struct sockaddr *)&neighbour_address[i], sizeof(neighbour_address[i]));
-			if (status > -1)
-			{
-				printf("Client Connected to port %d \n", neighbour_client_port[i]);
-
-				int send_status = send(client_socket[i], msg2, strlen(msg2), 0);
-
-				if (send_status > 48)
-				{
-					status = 1;
-					printf("Hello message sent\n");
-				}
-				else{
-					read(client_socket[i], buffer, 1024);
-					status = 0;
-				}
-			}
-			printf("Client done with someone\n");
-
-		}
-	}
-}
-
 int main(int argc, char **argv)
 {
 	////////////////////Processing
-	std::cout << "Starting up" << std::endl;
 
 	print_file_names(argv);
 
@@ -162,7 +50,7 @@ int main(int argc, char **argv)
 	int S_NO = atoi(out[2].c_str());
 	int PORT = atoi(out[1].c_str());
 	int ID = atoi(out[0].c_str());
-	printf("I am %d on port %d with id %d\n", ID, PORT, S_NO);
+	// printf("I am %d on port %d with id %d\n", ID, PORT, S_NO);
 	getline(read_file, line);
 	line.erase(std::remove(line.begin(), line.end(), '\r'), line.end());
 
@@ -197,20 +85,120 @@ int main(int argc, char **argv)
 
 	read_file.close();
 
-	/////////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////////////
 
-	std::thread t1(server, PORT, num_neighbour);
 
-	// server(server_socket, change, address, num_neighbour);
+	int client_or_server = fork();
 
-	std::thread t2(client, S_NO, num_neighbour, std::ref(neighbour_client_port),
-				   std::ref(neighbour_client_number), PORT, ID);
+	if (client_or_server == 0)
+	{
+		FILE *output_file;
+		output_file = fopen(argv[3], "w");
+		fprintf(output_file, "%d\n",int(client_or_server));
+		// client process
 
-	// client(S_NO, change, num_neighbour, neighbour_client_port,
-	// neighbour_client_number, PORT, ID);
+		// std::cout << "Client " << S_NO << " starting up. Num neighbours = "<<num_neighbour << std::endl;
 
-	t1.join();
-	t2.join();
+		struct sockaddr_in neighbour_address[num_neighbour];
+		int client_socket[num_neighbour];
+		int status[num_neighbour];
+
+		for (int i = 0; i < num_neighbour; i++)
+		{
+
+			int valread;
+			std::string msg = std::to_string(S_NO) + " You are " + std::to_string(neighbour_client_number[i]) + ". I am " + std::to_string(S_NO) + ", on port " + std::to_string(PORT) + ",with ID " + std::to_string(ID);
+			const char *msg2 = msg.c_str();
+			char buffer[1024] = {0};
+			client_socket[i] = socket(AF_INET, SOCK_STREAM, 0);
+
+			neighbour_address[i].sin_family = AF_INET;
+			neighbour_address[i].sin_port = htons(neighbour_client_port[i]);
+			neighbour_address[i].sin_addr.s_addr = inet_addr("127.0.0.1");
+			// printf("Client attempting connect to port %d \n", neighbour_client_port[i]);
+
+			int status = 0;
+			while (status == 0)
+			{
+				status = connect(client_socket[i], (struct sockaddr *)&neighbour_address[i], sizeof(neighbour_address[i]));
+				if (status > -1)
+				{
+					fprintf(output_file, "Client Connected to port %d \n", neighbour_client_port[i]);
+					int len_of_msg_remaining = strlen(msg2);
+					// int sent_till_now = 0;
+					int sent_status;
+					while (len_of_msg_remaining > 0)
+					{
+						sent_status = send(client_socket[i], msg2, len_of_msg_remaining, 0);
+						msg2 += sent_status;
+						len_of_msg_remaining -= sent_status;
+						// sent_till_now+=sent_status;
+					}
+					fprintf(output_file, "Client SENT data to port %d \n", neighbour_client_port[i]);
+					close(client_socket[i]);
+				}
+			}
+		}
+	}
+	else
+	{
+		FILE *output_file;
+		output_file = fopen(argv[4], "w");
+		fprintf(output_file, "%d\n",int(client_or_server));
+		fprintf(output_file, "hello from server\n");
+		// server process
+
+
+		char* buffer = new char[1024];
+		// Creating socket file descriptor
+
+		int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+		struct sockaddr_in address;
+		int addrlen = sizeof(address);
+		int yes = 1;
+		// std::cout << "Server "<< " made with fd as " << server_socket << "and port " << port << std::endl;
+
+		setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &yes, sizeof(yes));
+		address.sin_family = AF_INET;
+		address.sin_addr.s_addr = INADDR_ANY;
+		address.sin_port = htons(PORT);
+		bind(server_socket, (struct sockaddr *)&address, sizeof(address));
+		int count = 0;
+
+		listen(server_socket, 20);
+		while (count < num_neighbour)
+		{
+
+			// std::cout << "Server " << " listening on port " << port << std::endl;
+
+			int new_client = accept(server_socket, (struct sockaddr *)&address, (socklen_t *)&addrlen);
+			// std::cout << "Server accept returned " << new_client << std::endl;
+			fprintf(output_file, "CONNECTION AAYA\n\n");
+			if (new_client > -1)
+			{
+				fprintf(output_file, "Server accepted a connection\n");
+				
+				char* buf = buffer;
+				// int received_till_now = 0;
+				int client_msg_len=1;
+				int max_len = 1024;
+				if (client_msg_len!=0)
+				{
+					client_msg_len = recv(new_client, buffer, max_len, 0);
+					// received_till_now +=client_msg_len;
+					buffer+=client_msg_len;
+					max_len-=client_msg_len;
+				}
+				fprintf(output_file, "Server RECEIVED DATA on %d\n",new_client);
+
+				fprintf(output_file, "%s", buf);
+				buffer = buf;
+			}
+		}
+
+		fprintf(output_file, "Server exiting\n");
+	}
+
 
 	return 0;
 }
--- client.cpp
+++ client.cpp
@@ -8,10 +8,10 @@
 
 int main(int argc, char const *argv[])
 {
-    int PORT= 7000;
+    int PORT= 8888;
 	int sock = 0, valread;
 	struct sockaddr_in serv_addr;
-	char *hello = "CHUP";
+	char hello[] = ":exit";
 	char buffer[1024] = {0};
 	sock = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -21,8 +21,16 @@ int main(int argc, char const *argv[])
 	// Convert IPv4 and IPv6 addresses from text to binary form
 	// inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
 
-	connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
-	send(sock , hello , strlen(hello) , 0 );
+	int server = connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+
+	int data = send(sock , hello , strlen(hello) , 0 );
+	while(data ==0){
+		data = send(sock , hello , strlen(hello) , 0 );
+	}
+	while(true){
+		recv(server, buffer, sizeof(buffer), 0);		
+	}
 	printf("Hello message sent\n");
+	printf("hey\n");
 	return 0;
 }
