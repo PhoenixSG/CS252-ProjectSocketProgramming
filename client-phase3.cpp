48a4779283395866e926d8d60212d420f38cf2de
Shashwat Garg <shashwatg2002@gmail.com>
2022-04-13 18:08:47 +0530

    format

--- client-phase3.cpp
+++ client-phase3.cpp
@@ -15,8 +15,6 @@
 #include <fcntl.h>
 #include <sys/sendfile.h>
 #include <chrono>
-     
-
 
 void tokenize(std::string &str, char delim, std::vector<std::string> &out)
 {
@@ -48,7 +46,8 @@ std::vector<std::string> mod_get_file_names(std::string msg)
 	tokenize(otp, '\n', r1);
 	std::vector<std::string> retval;
 	std::vector<std::string> temp;
-	for(auto x : r1){
+	for (auto x : r1)
+	{
 		tokenize(x, '#', temp);
 		retval.push_back(temp[0]);
 		temp.clear();
@@ -56,53 +55,56 @@ std::vector<std::string> mod_get_file_names(std::string msg)
 	return retval;
 }
 
-std::map<std::string, off_t> file_size_map (std::string msg){
- 
- 	std::map<std::string, off_t> retval;
- 	auto otp = msg.substr(msg.find(";"));
- 	otp = &otp[1];
- 	std::vector<std::string> r1;
- 	tokenize(otp, '\n', r1);
- 	std::vector<std::string> temp;
- 	for(auto x : r1){
- 		tokenize(x, '#', temp);
- 		retval[temp[0]] = off_t(std::stoi(temp[1])); 
- 		temp.clear();
- 	}
- 
- 	return retval;
- 
+std::map<std::string, off_t> file_size_map(std::string msg)
+{
+
+	std::map<std::string, off_t> retval;
+	auto otp = msg.substr(msg.find(";"));
+	otp = &otp[1];
+	std::vector<std::string> r1;
+	tokenize(otp, '\n', r1);
+	std::vector<std::string> temp;
+	for (auto x : r1)
+	{
+		tokenize(x, '#', temp);
+		retval[temp[0]] = off_t(std::stoi(temp[1]));
+		temp.clear();
+	}
+
+	return retval;
 }
-void get_file_len (std::string file_to_send, off_t& len){
+void get_file_len(std::string file_to_send, off_t &len)
+{
 
-		struct stat file_stat;		
+	struct stat file_stat;
 
-
-		auto fd = open(file_to_send.c_str(), O_RDONLY);
-		if (fd == -1){
-			puts("Failed to open file");
-			return;
-		}
-        if (fstat(fd, &file_stat) < 0){
-			puts("Failed to do file stat");
-			return;        
-		}
-		len = file_stat.st_size;
-
+	auto fd = open(file_to_send.c_str(), O_RDONLY);
+	if (fd == -1)
+	{
+		puts("Failed to open file");
+		return;
+	}
+	if (fstat(fd, &file_stat) < 0)
+	{
+		puts("Failed to do file stat");
+		return;
+	}
+	len = file_stat.st_size;
 }
 
-std::string repackage (std::string file_names, std::string path){
- 
- 	std::vector<std::string> r;
- 	tokenize(file_names, '\n', r);
- 	off_t len = 0;
- 	std::string retval = "";
- 	for(auto x : r){
- 		get_file_len(path + x, len);
- 		retval += (x + "#" + std::to_string(int(len)) + "\n");
- 
- 	}
- 	return retval;
+std::string repackage(std::string file_names, std::string path)
+{
+
+	std::vector<std::string> r;
+	tokenize(file_names, '\n', r);
+	off_t len = 0;
+	std::string retval = "";
+	for (auto x : r)
+	{
+		get_file_len(path + x, len);
+		retval += (x + "#" + std::to_string(int(len)) + "\n");
+	}
+	return retval;
 }
 
 std::string GetStdoutFromCommand(std::string cmd)
@@ -137,25 +139,26 @@ std::string process(std::string const &s)
 	}
 }
 
-std::vector<std::string> intersection(std::vector<std::string> &v1, std::vector<std::string> &v2){
-	
+std::vector<std::string> intersection(std::vector<std::string> &v1, std::vector<std::string> &v2)
+{
+
 	std::vector<std::string> v3;
 	std::sort(v1.begin(), v1.end());
 	std::sort(v2.begin(), v2.end());
 	std::set_intersection(v1.begin(), v1.end(), v2.begin(), v2.end(), back_inserter(v3));
-	
+
 	return v3;
 }
 
-std::string piece_together (std::vector<std::string> v, char delim){
+std::string piece_together(std::vector<std::string> v, char delim)
+{
 	std::string retval = "";
-	if(v.size() == 0)
-	    return retval;
-	for(int i = 0; i < v.size() - 1; ++i)
+	if (v.size() == 0)
+		return retval;
+	for (int i = 0; i < v.size() - 1; ++i)
 		retval += (v[i] + delim);
 	retval += v[v.size() - 1];
 	return retval;
-
 }
 /*
 int index (int* client_socket, int x){
@@ -167,9 +170,10 @@ int index (int* client_socket, int x){
 
 }
 */
-bool all_true (bool* arr, int n){
-	for(int i = 0; i < n; ++i)
-		if(!arr[i])
+bool all_true(bool *arr, int n)
+{
+	for (int i = 0; i < n; ++i)
+		if (!arr[i])
 			return false;
 	return true;
 }
@@ -184,9 +188,6 @@ void clean_map (std::map<int, std::vector<std::string>> &server_map){
 
 }*/
 
-
-
-
 void client(int S_NO, int num_neighbour, std::vector<int> &neighbour_client_port, std::vector<int> &neighbour_client_number, int PORT, int ID, std::string path)
 {
 	struct sockaddr_in neighbour_address[num_neighbour];
@@ -197,8 +198,7 @@ void client(int S_NO, int num_neighbour, std::vector<int> &neighbour_client_port
 	{
 
 		int valread;
-		msg = "Connected to " + std::to_string(ID) + " with unique-ID " + std::to_string(S_NO) + " on port " 
-		+ std::to_string(PORT) + ";" + repackage(GetStdoutFromCommand("cd " + path + " ;ls -p -1v | grep -v /"), path);
+		msg = "Connected to " + std::to_string(ID) + " with unique-ID " + std::to_string(S_NO) + " on port " + std::to_string(PORT) + ";" + repackage(GetStdoutFromCommand("cd " + path + " ;ls -p -1v | grep -v /"), path);
 		const char *msg2 = msg.c_str();
 		char buffer[1024] = {0};
 		client_socket[i] = socket(AF_INET, SOCK_STREAM, 0);
@@ -215,68 +215,65 @@ void client(int S_NO, int num_neighbour, std::vector<int> &neighbour_client_port
 			if (status > -1)
 			{
 				send(client_socket[i], msg2, strlen(msg2), 0);
-				//close(client_socket[i]);
+				// close(client_socket[i]);
 			}
 		}
 	}
-	
-	//std::cout << msg ;
-	//receive loop
+
+	// std::cout << msg ;
+	// receive loop
 	std::map<int, std::vector<std::string>> server_map;
-	for (int i = 0; i < num_neighbour; i++){
+	for (int i = 0; i < num_neighbour; i++)
+	{
 		char buffer[1000] = {'\0'};
-		recv(client_socket[i],buffer,1000,0);
+		recv(client_socket[i], buffer, 1000, 0);
 		std::string m = buffer;
 		std::vector<std::string> x;
 		auto c = m[0];
 		m.erase(m.begin());
-		tokenize(m,'\n',x);
-		auto l = piece_together(x,' ');
-		
+		tokenize(m, '\n', x);
+		auto l = piece_together(x, ' ');
+
 		server_map[c] = x;
-		
+
 		std::cout << c << ", " << l << std::endl;
 	}
 
-
-	
-	//send loop
+	// send loop
 	/*
 	int cnt = 0;
 	for(auto x : server_map){
 
 		ssize_t len;
-        int fd, offset = 0, sent_bytes = 0;
+		int fd, offset = 0, sent_bytes = 0;
 		off_t remain_data;
-        char file_size[256];
-        struct stat file_stat;
-		
+		char file_size[256];
+		struct stat file_stat;
+
 		for(auto y : x.second){
 			get_file_len(path + y, remain_data);
 			fd = open((path + y).c_str(), O_RDONLY);
 			std::cout << y << std::endl;
 			while (((sent_bytes = sendfile(client_socket[cnt], fd, (off_t*)&offset, BUFSIZ)) > 0) && (remain_data > 0)){
 				std::cout << "1. Server sent " << sent_bytes <<  " bytes from file's data, offset is now : "
-				<< offset << " and remaining data = " << remain_data << "\n"; 
-				
+				<< offset << " and remaining data = " << remain_data << "\n";
+
 				remain_data -= sent_bytes;
 				std::cout << "2. Server sent " << sent_bytes <<  " bytes from file's data, offset is now : "
-				<< offset << " and remaining data = " << remain_data << "\n"; 
+				<< offset << " and remaining data = " << remain_data << "\n";
 			}
 
 		}
 		++cnt;
 	}
-	*/	
-std::cout<<S_NO<<"    HEIHNFHOIENFUIENCIUEBNFIUEB\n\n\n";
-
-
+	*/
+	std::cout << S_NO << "    HEIHNFHOIENFUIENCIUEBNFIUEB\n\n\n";
 }
 
-
-void display (std::vector<int>& neighbour_client_number){
-	for(auto x : neighbour_client_number)
-		std:: cout << x << " ";
+void display(std::vector<int> &neighbour_client_number)
+{
+	for (auto x : neighbour_client_number)
+		std::cout << x << " ";
 	std::cout << std::endl;
 }
 
@@ -289,16 +286,15 @@ void display (std::vector<int>& neighbour_client_number){
 // 	std::cout << "\nFile client map printed\n";
 // }
 
-void display (std::map<std::string, int> m){
-	for(auto x : m)
+void display(std::map<std::string, int> m)
+{
+	for (auto x : m)
 		std::cout << "(" << x.first << ", " << x.second << "), ";
 	std::cout << std::endl;
 }
 
-
-
-void server(int PORT, std::vector<std::string> files_to_download, int num_neighbours, 
-			std::vector<int>& neighbour_client_number, int ID)
+void server(int PORT, std::vector<std::string> files_to_download, int num_neighbours,
+			std::vector<int> &neighbour_client_number, int ID)
 {
 	int opt = 1;
 	int master_socket, addrlen, new_socket, client_socket[30], max_clients = 30, activity, i, valread, sd;
@@ -339,14 +335,11 @@ void server(int PORT, std::vector<std::string> files_to_download, int num_neighb
 
 	addrlen = sizeof(address);
 
-
-
 	std::map<std::string, int> file_map;
 	std::map<std::string, int> file_map_no;
 	std::map<int, std::string> m;
 	std::map<std::string, off_t> file_sz_map;
 	std::vector<std::string> file_client_map[30];
-
 
 	while (num_connections < num_neighbours)
 	{
@@ -386,8 +379,6 @@ void server(int PORT, std::vector<std::string> files_to_download, int num_neighb
 			}
 		}
 
-		
-		
 		for (i = 0; i < max_clients; i++)
 		{
 			sd = client_socket[i];
@@ -414,11 +405,11 @@ void server(int PORT, std::vector<std::string> files_to_download, int num_neighb
 					s >> temp >> temp >> word2 >> temp >> temp >> word;
 
 					auto file_names = mod_get_file_names(str2);
-					
+
 					auto x = file_size_map(str2);
 					file_sz_map.insert(x.begin(), x.end());
 					auto files_available = intersection(file_names, files_to_download);
-					//file_client_map[std::stoi(word2)-1] = files_available;
+					// file_client_map[std::stoi(word2)-1] = files_available;
 
 					for (auto x : files_available)
 					{
@@ -430,63 +421,57 @@ void server(int PORT, std::vector<std::string> files_to_download, int num_neighb
 						}
 						else
 						{
-							//file_map[x] = std::min(w, file_map[x]);
-							//file_map_no[x] = 
-							if(w < file_map[x]){
+							// file_map[x] = std::min(w, file_map[x]);
+							// file_map_no[x] =
+							if (w < file_map[x])
+							{
 								file_map[x] = w;
 								file_map_no[x] = w2;
 							}
-							
 						}
 					}
 
-					auto prnt = process(str)+ "\n"; 
+					auto prnt = process(str) + "\n";
 
-					
 					m[atoi(word2.c_str())] = prnt;
 				}
 			}
 		}
+	}
 
-		
-	}
-	
-	
-	for(auto x : file_map_no){
-		
+	for (auto x : file_map_no)
+	{
+
 		file_client_map[x.second - 1].push_back(x.first);
-		
 	}
-	//std::cout << ID << "     "; display(file_client_map);
-	//send loop
+	// std::cout << ID << "     "; display(file_client_map);
+	// send loop
 	int sz = neighbour_client_number.size();
 	bool all_sent[sz];
 
-	for(int i = 0; i < sz; ++i)
+	for (int i = 0; i < sz; ++i)
 		all_sent[i] = false;
-	//std::cout << "Node number " << ID << " is still alive1\n";
-	while(!all_true(all_sent, sz)){
-		
-		for(int i = 0; i < sz ; ++i){
+	// std::cout << "Node number " << ID << " is still alive1\n";
+	while (!all_true(all_sent, sz))
+	{
+
+		for (int i = 0; i < sz; ++i)
+		{
 			ssize_t by_sent;
-			if(!all_sent[i]){
-				std::string blah = std::to_string(ID) + piece_together(file_client_map[neighbour_client_number[i] - 1],'\n');
-				const char* x = blah.c_str();
-				by_sent = send(client_socket[i],x,strlen(x),0);       
+			if (!all_sent[i])
+			{
+				std::string blah = std::to_string(ID) + piece_together(file_client_map[neighbour_client_number[i] - 1], '\n');
+				const char *x = blah.c_str();
+				by_sent = send(client_socket[i], x, strlen(x), 0);
 			}
-			if(by_sent != -1)
-				all_sent[i] = true;	
-			
+			if (by_sent != -1)
+				all_sent[i] = true;
 		}
 	}
-	
 
-	//std::cout << "Node number " << ID << " is still alive2\n";
+	// std::cout << "Node number " << ID << " is still alive2\n";
 
-
-
-
-	//receive loop
+	// receive loop
 	/*
 	std::string mkdir = "mkdir -p " + path + "Downloaded";
 	system(mkdir.c_str());
@@ -505,40 +490,35 @@ void server(int PORT, std::vector<std::string> files_to_download, int num_neighb
 
 				else
 				{
-					
+
 				}
 			}
 		}
 	*/
-		
-	
-
-
 
 	/* clean this up
 	recv(client_socket, buffer, BUFSIZ, 0);
-        file_size = atoi(buffer);
-        //fprintf(stdout, "\nFile size : %d\n", file_size);
+		file_size = atoi(buffer);
+		//fprintf(stdout, "\nFile size : %d\n", file_size);
 
-        received_file = fopen(FILENAME, "w");
-        if (received_file == NULL)
-        {
-                fprintf(stderr, "Failed to open file foo --> %s\n", strerror(errno));
+		received_file = fopen(FILENAME, "w");
+		if (received_file == NULL)
+		{
+				fprintf(stderr, "Failed to open file foo --> %s\n", strerror(errno));
 
-                exit(EXIT_FAILURE);
-        }
+				exit(EXIT_FAILURE);
+		}
 
-        remain_data = file_size;
+		remain_data = file_size;
 
-        while ((remain_data > 0) && ((len = recv(client_socket, buffer, BUFSIZ, 0)) > 0))
-        {
-                fwrite(buffer, sizeof(char), len, received_file);
-                remain_data -= len;
-                fprintf(stdout, "Receive %d bytes and we hope :- %d bytes\n", len, remain_data);
-        }
-        fclose(received_file);
+		while ((remain_data > 0) && ((len = recv(client_socket, buffer, BUFSIZ, 0)) > 0))
+		{
+				fwrite(buffer, sizeof(char), len, received_file);
+				remain_data -= len;
+				fprintf(stdout, "Receive %d bytes and we hope :- %d bytes\n", len, remain_data);
+		}
+		fclose(received_file);
 	*/
-       
 
 	for (auto x : files_to_download)
 	{
@@ -548,19 +528,18 @@ void server(int PORT, std::vector<std::string> files_to_download, int num_neighb
 		}
 	}
 
-	
-	for(auto x:m){
+	for (auto x : m)
+	{
 		std::cout << x.second;
 	}
-	
 
 	for (auto i = file_map.begin(); i != file_map.end(); ++i)
 	{
 		std::string s1 = ("Found " + i->first + " at " + std::to_string(i->second) + " with MD5 0 at depth 1\n");
 		std::cout << s1;
 	}
-std::cout<<PORT<<"        HEIHNFHOIENFUIENCIUEBNFIUEB\n\n\n";
-	//std::cout << "Node number " << ID << " is still alive\n";
+	std::cout << PORT << "        HEIHNFHOIENFUIENCIUEBNFIUEB\n\n\n";
+	// std::cout << "Node number " << ID << " is still alive\n";
 }
 
 int main(int argc, char **argv)
@@ -627,7 +606,6 @@ int main(int argc, char **argv)
 
 	t1.join();
 	t2.join();
-	
 
 	return 0;
 }
